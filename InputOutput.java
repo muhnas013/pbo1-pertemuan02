bfa45bf3c7c5cb8de8eb0414352eb91895ed6a0c
muhnas013 <muhammad.nasrullah1993@gmail.com>
2023-05-19 17:38:59 +0800

    Compile & Run

--- InputOutput.java
+++ InputOutput.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class InputOutput {
-    public static void (String [] args) {
+    public static void main(String [] args) {
         Scanner scanner = new Scanner(System.in);
         String nama;
         System.out.print("Masukan Nama Anda : ");
