bdcfec13b1deea5ba0dc46f1117445a1925fa210
muhnas013 <muhammad.nasrullah1993@gmail.com>
2023-05-19 17:32:57 +0800

    Buat Input String Kedalam Variable

--- model/InputOutput.java
+++ model/InputOutput.java
@@ -3,5 +3,8 @@ import java.util.Scanner;
 public class InputOutput {
     public static void (String [] args) {
         Scanner scanner = new Scanner(System.in);
+        String nama;
+        System.out.print("Masukan Nama Anda : ");
+        nama = scanner.nextLine();
         }
 }
